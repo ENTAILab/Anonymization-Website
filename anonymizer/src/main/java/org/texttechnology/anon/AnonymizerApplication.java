f12ff6d6d1233e231021180cf7effd8e960fffeb
sittardt <cssittardt@gmail.com>
2026-07-15 17:51:57 +0200

    init

---
+++ anonymizer/src/main/java/org/texttechnology/anon/AnonymizerApplication.java
@@ -0,0 +1,15 @@
+package org.texttechnology.anon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymizerApplication.class, args);
+	}
+
+}
+
+
---
+++ anonymizer/src/test/java/org/texttechnology/anon/AnonymizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.texttechnology.anon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
